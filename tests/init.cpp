5aa550f4b165ac6bb8673fc3d8e0a3bda72bf591
elinagabitova <elinagabitovaa@gmail.com>
2017-05-12 00:27:48 +0500

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,31 +1,121 @@
-#include <iteratorsort.hpp>
+#include <tree.h>
+
 #include <catch.hpp>
 
-SCENARIO ("array")
+
+
+SCENARIO ("init", "[init]")
+
 {
-  int a[] = {3, 2, 4, 5, 6};
-  int* s=a;
-  int* l=a+5;
- insertion_sort(s,l);
- REQUIRE(a[0] == 2);
- REQUIRE(a[1] == 3);
- REQUIRE(a[2] == 4);
- REQUIRE(a[3] == 5);
- REQUIRE(a[4] == 6);
+
+  Tree<int> test;
+
+  REQUIRE(test.root_() == nullptr);
+
+  REQUIRE(test.get_count() == 0);
+
 }
-SCENARIO("matr")
+
+SCENARIO("insert", "[init]")
+
 {
-  int arr[3][3] = {4, 21, 12, 5, 2, 7, 42, 26, 8};
-  int * beg = &arr[0][0];
-  int * end = &arr[2][3];
-  insertion_sort(beg,end);
-  REQUIRE(arr[0][0] == 2);
-  REQUIRE(arr[0][1] == 4);
-  REQUIRE(arr[0][2] == 5);
-  REQUIRE(arr[1][0] == 7);
-  REQUIRE(arr[1][1] == 8);
-  REQUIRE(arr[1][2] == 12);
-  REQUIRE(arr[2][0] == 21);
-  REQUIRE(arr[2][1] == 26);
-  REQUIRE(arr[2][2] == 42);
+
+  Tree<int> test;
+
+  test.insert_node(5);
+
+  REQUIRE(test.get_count() == 1);
+
+  REQUIRE(test.find_node(5, test.root_())->key == 5);
+
+}
+
+SCENARIO("find_node", "[init]")
+
+{
+
+  Tree<int> test;
+
+  test.insert_node(4);
+
+  REQUIRE(test.find_node(4, test.root_()) != nullptr);
+
+  REQUIRE(test.find_node(4, test.root_())->key == 4);
+
+}
+
+SCENARIO("get root", "[init]")
+
+{
+
+  Tree<int> test;
+
+  test.insert_node(4);
+
+  REQUIRE(test.root_() != 0);
+
+}
+
+SCENARIO("insert equal elements", "[insert]")
+
+ {
+
+   Tree<int> test;
+
+   test.insert_node(4);
+
+   test.insert_node(4);
+
+   REQUIRE(test.get_count() == 1);
+
+ }
+
+SCENARIO ("reading/writing", "[init]")
+
+{
+
+  Tree<int> test1;
+
+  test1.insert_node(4);
+
+  test1.insert_node(3);
+
+  test1.writing("file2.txt");
+
+  Tree<int> test2;
+
+  test2.reading("file2.txt");
+
+  REQUIRE(test2.find_node(4, test2.root_())!= nullptr);
+
+  REQUIRE(test2.find_node(3, test2.root_())!= nullptr);
+
+  REQUIRE(test1.get_count() == test2.get_count());
+
+}
+
+SCENARIO("deleteX")
+
+{
+
+  Tree<int> test;
+
+  test.insert_node(6);
+
+  test.insert_node(7);
+
+  test.insert_node(9);
+
+  test.deleteVal(6);
+
+  test.deleteVal(9);
+
+  REQUIRE(test.find_node(9, test.root_())== nullptr);
+
+  REQUIRE(test.find_node(7, test.root_())== test.root_());
+
+  REQUIRE(test.root_() != nullptr);
+
+  REQUIRE(test.get_count() == 1);
+
 }
