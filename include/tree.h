05a6b80b8bff8ae612b7b9110cd93f81800ab72c
elinagabitova <elinagabitovaa@gmail.com>
2017-05-17 00:32:57 +0500

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -176,7 +176,7 @@ template<typename T>
 void Tree<T>::write(std::ofstream& fout)const
 { 
 		if (!fout.is_open())
-			throw runtime_error;
+			throw exception::runtime_error;
 		fout << count << " ";
 		output(fout, root);
 		fout.close();
